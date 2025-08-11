fa35d10709a9c26349320275cf0a54e580ddd29e
nariman <narik11102004@gmail.com>
2025-08-12 00:27:51 +0300

    add controller

--- src/main/java/nesalmanov/ru/springdocker/controller/HelloController.java
+++ src/main/java/nesalmanov/ru/springdocker/controller/HelloController.java
@@ -1,4 +1,14 @@
 package nesalmanov.ru.springdocker.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping
+    public String getHello() {
+        return "Hello!";
+    }
+
 }
